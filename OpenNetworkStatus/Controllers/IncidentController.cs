f8590612968bd24e973744df1d6be248e83f3915
Patrick Falk Nielsen <patrick.nielsen@bestseller.com>
2020-06-25 10:19:10 +0200

    Basic Incident page as described in #7

---
+++ OpenNetworkStatus/Controllers/IncidentController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using OpenNetworkStatus.Models;
+using OpenNetworkStatus.Services.IncidentServices.Queries;
+
+namespace OpenNetworkStatus.Controllers
+{
+    public class IncidentController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public IncidentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [Route("[controller]/{incidentId}")]
+        public async Task<IActionResult> GetById([FromRoute]GetIncidentByIdQuery incidentQuery)
+        {
+            var incidentResource = await _mediator.Send(incidentQuery);
+            if (incidentResource == null)
+            {
+                return NotFound();
+            }
+
+            return View(new IncidentViewModel(incidentResource.Impact, incidentResource.Title, incidentResource.Updates));
+        }
+    }
+}
---
+++ OpenNetworkStatus/Models/IncidentViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using OpenNetworkStatus.Data.Enums;
+using OpenNetworkStatus.Services.IncidentServices.Resources;
+
+namespace OpenNetworkStatus.Models
+{
+    public class IncidentViewModel
+    {
+        public IncidentImpact Impact { get; set; }
+
+        public string Title { get; set; }
+
+        public List<IncidentUpdateResource> Updates { get; set; }
+
+        public IncidentViewModel(IncidentImpact impact, string title, List<IncidentUpdateResource> updates)
+        {
+            Impact = impact;
+            Title = title;
+            Updates = updates;
+        }
+    }
+}
