2453acf17e334d62f7d8f34bfeaa4ad4d36dcc34
Patrick Falk Nielsen <patrick.nielsen@bestseller.com>
2020-09-11 12:38:02 +0200

    Added support for two columns for components

--- OpenNetworkStatus/Models/Options/SiteOptions.cs
+++ OpenNetworkStatus/Models/Options/SiteOptions.cs
@@ -7,6 +7,8 @@ namespace OpenNetworkStatus.Models.Options
         public string Name { get; set; } = "OpenNetworkStatus";
 
         public bool EnableSwagger { get; set; } = false;
+
+        public bool TwoColumns { get; set; } = false;
         
         public CorsOptions Cors { get; set; }
         
